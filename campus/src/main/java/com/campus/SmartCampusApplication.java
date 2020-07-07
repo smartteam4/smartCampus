bdd9425cede6f4174cdaaca2225892da565dbc1e
cuixin3266 <cuixin3266@users.noreply.github.com>
2020-07-07 14:43:21 +0800

    更新JPA，开启DDL

--- campus/src/main/java/com/campus/SmartCampusApplication.java
+++ campus/src/main/java/com/campus/SmartCampusApplication.java
@@ -1,28 +1,15 @@
 package com.campus;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
-import com.campus.storage.StorageProperties;
-import com.campus.storage.StorageService;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class SmartCampusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCampusApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
 
 }
